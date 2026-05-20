27865c55600e629bbc6492716dc8378314979e01
Anwesh Biswas <anweshbiswas@Anweshs-MacBook-Air.local>
2026-05-20 15:24:55 +0530

    fix(main): resolve ReferenceError Link is not defined and rollback navbar design

--- components/MainPage.tsx
+++ components/MainPage.tsx
@@ -1,4 +1,5 @@
 import React, { Suspense, lazy } from 'react';
+import { Link } from 'react-router-dom';
 import HowItWorksStepper from './HowItWorksStepper';
 import SEO from './SEO';
 import { useSupabase } from './SupabaseProvider';
